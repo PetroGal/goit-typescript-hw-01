75512056a40671afeac7a27e7584f60fb21a5e92
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-08-10 11:23:14 +0300

    final commit

--- src/generics/2.ts
+++ src/generics/2.ts
@@ -4,22 +4,31 @@ type AllType = {
   color: string
   weight: number
 }
+// 1st mentor's variant
+function compare<T extends Pick<AllType, keyof AllType>>(top: T, bottom: T) {}
 
-function compare<T extends keyof AllType>(
-  top: Pick<AllType, T>,
-  bottom: Pick<AllType, T>
-): Pick<AllType, T> {
-  const result = {} as Pick<AllType, T>
+// 2nd mentor's variant
+// function compare<T extends AllType>(
+//   top: Pick<T, 'color' | 'name'>,
+//   bottom: Pick<T, 'position' | 'weight'>
+// ): AllType {}
 
-  for (const key in top) {
-    result[key as T] = top[key as T]
-  }
+// my variant
+// function compare<T extends keyof AllType>(
+//   top: Pick<AllType, T>,
+//   bottom: Pick<AllType, T>
+// ): Pick<AllType, T> {
+//   const result = {} as Pick<AllType, T>
 
-  for (const key in bottom) {
-    if (!(key in result)) {
-      result[key as T] = bottom[key as T]
-    }
-  }
+//   for (const key in top) {
+//     result[key as T] = top[key as T]
+//   }
 
-  return result
-}
+//   for (const key in bottom) {
+//     if (!(key in result)) {
+//       result[key as T] = bottom[key as T]
+//     }
+//   }
+
+//   return result
+// }
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,7 @@
+// mentor's variant
+// function merge<T extends object, U extends object>(objA: T, objB: U): T & U {}
+
+//my variant
 function merge<T, U>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB)
 }
